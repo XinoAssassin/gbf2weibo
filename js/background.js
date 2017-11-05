585e89628797bcf1016ed27990967f2ed0a97a50
XinoAssassin Ni <phnjs211@outlook.com>
2017-11-05 17:54:05 +0800

    add guide page

---
+++ js/background.js
@@ -0,0 +1,10 @@
+function install_notice() {
+    if (localStorage.getItem('install_time'))
+        return;
+
+    var now = new Date().getTime();
+    localStorage.setItem('install_time', now);
+    chrome.tabs.create({url: "guide.html"});
+}
+
+install_notice();
